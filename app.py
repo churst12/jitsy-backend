f234e40159464a6d5b8d81d0429c742550e19c7e
Collin Hurst <collinhurst12@gmail.com>
2018-11-04 02:51:46 -0800

    worker done

--- app.py
+++ app.py
@@ -12,33 +12,41 @@ ma = Marshmallow(app)
 
 import models
 
-class UserSchema(ma.Schema):
+class WorkerSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('nickname', 'email')
+        fields = ('name', 'email', 'phone_number', 'experiences','skills','location','profile_img','bio','links','certifications')
 
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+
+worker_schema = WorkerSchema()
+workers_schema = WorkerSchema(many=True)
 
 
 # endpoint to create new user
-@app.route("/user", methods=["POST"])
-def add_user():
+@app.route("/worker", methods=["POST"])
+def add_worker():
     email = request.json['email']
-    nickname = request.json['nickname']
+    name = request.json['name']
+    phone_number = request.json['phone_number']
+    experiences = request.json['experiences']
+    skills = request.json['skills']
+    location = request.json['location']
+    profile_img = request.json['profile_img']
+    bio = request.json['bio']
+    links = request.json['links']
+    certifications = request.json['certifications']
     
-    
-    new_user = models.User(nickname, email)
+    new_worker = models.Worker(name, email,phone_number, experiences,skills,location,profile_img,bio,links,certifications)
 
-    db.session.add(new_user)
+    db.session.add(new_worker)
     db.session.commit()
 
     return "201 Created"
-@app.route('/user', methods=["GET"])
-def get_users():
-    all_users = models.User.query.all()
-    result = users_schema.dump(all_users)
+@app.route('/worker', methods=["GET"])
+def get_workers():
+    all_workers = models.Worker.query.all()
+    result = workers_schema.dump(all_workers)
     return jsonify(result.data)
 @app.route('/')
 def home():
--- models.py
+++ models.py
@@ -3,26 +3,50 @@ from app import db
 class Listing(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    business_id = db.Column(db.Integer)
-    date_start = db.Column(db.DateTime())
-    date_end = db.Column(db.DateTime())
-    category = db.Column(db.String())
-    location = db.Column(db.String())
-    price = db.Column(db.Integer)
-    description = db.Column(db.String())
-    skills = db.Column(db.String())
+    business_id = db.Column(db.Integer, nullable=True)
+    date_start = db.Column(db.DateTime(), nullable=True)
+    date_end = db.Column(db.DateTime(), nullable=True)
+    category = db.Column(db.String(), nullable=True)
+    location = db.Column(db.String(), nullable=True)
+    price = db.Column(db.Integer, nullable=True)
+    description = db.Column(db.String(), nullable=True)
+    skills = db.Column(db.String(), nullable=True)
 
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+    def __init__(self, business_id, date_start, date_end, category, location, price, description, skills):
+        self.business_id = business_id
+        self.date_start = date_start
+        self.date_end = date_end
+        self.category = category
+        self.location = location
+        self.price = price
+        self.description = description
+        self.skills = skills
 
 
 
-class User(db.Model):
+class Worker(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64) )
-    email = db.Column(db.String(120), index=True, unique=True)
+    name = db.Column(db.String(64) , nullable=True)
+    email = db.Column(db.String(120), index=True, nullable=True)
+    phone_number = db.Column(db.String(), nullable=True)
+    experiences = db.Column(db.String(), nullable=True)
+    skills = db.Column(db.String(), nullable=True)
+    location = db.Column(db.String(), )
+    profile_img = db.Column(db.String())
+    bio = db.Column(db.String(), nullable=True)
+    links = db.Column(db.String(), nullable=True)
+    certifications = db.Column(db.String(), nullable=True)
 
-    def __init__(self, nickname, email):
-        self.nickname = nickname
+
+    def __init__(self, name, email, phone_number, experiences, skills, location, profile_img, bio, links, certifications):
+        self.name = name
         self.email = email
+        self.phone_number = phone_number
+        self.experiences = experiences
+        self.skills = skills
+        self.location = location
+        self.profile_img = profile_img
+        self.bio = bio
+        self.links = links
+        self.certifications = certifications
