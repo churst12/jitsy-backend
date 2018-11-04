74e7fafbae8c26f3f9db12f0dde516deec2290c6
Collin Hurst <collinhurst12@gmail.com>
2018-11-03 23:53:26 -0700

    edit

--- app.py
+++ app.py
@@ -3,29 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jitsy.db'
+app.congfig['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import User, Listing
 
-@app.route('/add/')
-def webhook():
-    name = "ram"
-    email = "ram@ram.com"
-    u = User(id = id, nickname = name, email = email)
-    print("user created", u)
-    db.session.add(u)
-    db.session.commit()
-    return "user created"
-
-@app.route('/delete/')
-def delete():
-    u = User.query.get(i)
-    db.session.delete(u)
-    db.session.commit()
-    return "user deleted"
-
-if __name__ == '__main__':
-    app.run()
 
 @app.route('/')
 def home():
