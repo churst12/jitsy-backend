e93c617626c12cfa0782455b05daa859234e5928
Collin Hurst <collinhurst12@gmail.com>
2018-11-03 23:57:08 -0700

    edit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jitsy.db'
-app.congfig['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import User, Listing
